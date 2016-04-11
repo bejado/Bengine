5bd6aade8f10ad10ac6697f37d7314ec30282520
Ben Doherty <bdoherty@usc.edu>
2016-04-10 20:06:58 -0700

    Got a window working

---
+++ Engine/main/src/main.cpp
@@ -0,0 +1,111 @@
+#include <Windows.h>
+
+//--------------------------------------------------------------------------------------
+// Global Variables
+//--------------------------------------------------------------------------------------
+HINSTANCE               g_hInst = nullptr;
+HWND                    g_hWnd = nullptr;
+
+//--------------------------------------------------------------------------------------
+// Forward declarations
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow );
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//--------------------------------------------------------------------------------------
+// Register class and create window
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow )
+{
+    // Register class
+    WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(WNDCLASSEX));
+    wcex.cbSize = sizeof( WNDCLASSEX );
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
+    wcex.hbrBackground = ( HBRUSH )( COLOR_WINDOW + 1 );
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"WindowClass";
+    if( !RegisterClassEx( &wcex ) )
+        return E_FAIL;
+
+    // Create window
+    g_hInst = hInstance;
+    RECT rc = { 0, 0, 800, 600 };
+    AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE );
+    g_hWnd = CreateWindow( L"WindowClass", L"Ben's Game Engine",
+                           WS_OVERLAPPEDWINDOW,
+                           CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+                           nullptr );
+	if( !g_hWnd )
+	{
+		return E_FAIL;
+	}
+
+    ShowWindow( g_hWnd, nCmdShow );
+
+    return S_OK;
+}
+
+//--------------------------------------------------------------------------------------
+// Entry point to the program. Initializes everything and goes into a message processing 
+// loop. Idle time is used to render the scene.
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
+{
+    UNREFERENCED_PARAMETER( hPrevInstance );
+    UNREFERENCED_PARAMETER( lpCmdLine );
+
+	if( FAILED( InitWindow( hInstance, nCmdShow ) ) )
+	{
+		return 0;
+	}
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	DestroyWindow( g_hWnd );
+
+	return 0;
+}
+
+//--------------------------------------------------------------------------------------
+// Called every time the application receives a message
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM inWParam, LPARAM inLParam )
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch( message )
+    {
+        case WM_PAINT:
+            hdc = BeginPaint( hWnd, &ps );
+            EndPaint( hWnd, &ps );
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage( 0 );
+            break;
+			
+		case WM_KEYDOWN:
+			if( ( inLParam & ( 1 << 30 ) ) == 0 )
+			{
+				//great, it just went down!
+				// InputManager::Get().OnKeyEvent( EIA_KeyDown, inWParam );
+			}
+			break;
+			
+        default:
+			return DefWindowProc( hWnd, message, inWParam, inLParam );
+    }
+
+    return 0;
+}
