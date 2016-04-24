37bac177d8b049a2ee13d474129df7b6eaa974d8
Ben Doherty <benjdoherty15@gmail.com>
2016-04-24 15:59:22 -0700

    Removed unused files

--- Engine/core/inc/itpmath.h
+++ Engine/core/inc/itpmath.h
@@ -2,11 +2,6 @@
 #ifndef _MATH_H_
 #define _MATH_H_
 
-// Change this to 0 if you don't want to use SIMD
-#define USE_SIMD 1
-
-#if USE_SIMD
-
 #include "fastmath.h"
 namespace ITP485
 {
@@ -14,19 +9,6 @@ namespace ITP485
 	typedef FastMatrix4 Matrix4;
 	typedef FastQuaternion Quaternion;
 }
-
-#else
-
-#include "slowmath.h"
-namespace ITP485
-{
-	typedef SlowVector3 Vector3;
-	typedef SlowMatrix4 Matrix4;
-	typedef SlowQuaternion Quaternion;
-}
-
-#endif // USE_SIMD
-
 
 namespace ITP485
 {
